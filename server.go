70d8ce19c940d30afcdd84ce2eeedb2f8a60d74b
despossivel <mattbmoller@gmail.com>
2020-06-30 18:48:43 -0300

    containers

--- server.go
+++ server.go
@@ -11,5 +11,5 @@ func main() {
 }
 
 func Greeting(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
+    fmt.Fprintf(w, "<b>Hello, %s! </b>", r.URL.Path[1:])
 }
